682dbae1fd6c851c6f6bbb1f75db392e6f581cee
aka-engineer <aka2engineer@gmail.com>
2024-10-26 12:19:32 +0500

    buttons interface chanched

--- btn_iface.py
+++ btn_iface.py
@@ -12,18 +12,49 @@ class ButtonIfaceThread(Thread):
         'green': [0, 255, 0],
     }
 
-    _redPin = 12
-    _bluePin = 13
-    _greenPin = 14
-    _buzzerPin = 15
-
+    _R_PIN = 16
+    _B_PIN = 3
+    _G_PIN = 8
+    _BUZZ_PIN = 5
+    _START_PIN = 4
+    
+    _MODE_PIN = 15
+    _IR_PIN = 2
+    _PMP_PIN1 = 39
+    _PMP_PIN2 = 40
+    _PMP_PIN3 = 41
+    _PMP_PIN4 = 42
+    
+    _RXPIN = 35
+    _TXPIN = 45
+    
+    _BTN_WTROUT_PIN = 48
+    _BTN_WTRIN_PIN = 47
+    _BTN_LED_PIN = 21
+    _BTN_PWR_PIN = 20
+    _BTN_FLM_PIN = 19
+    
+    _LVL11_PIN = 46
+    _LVL12_PIN = 9
+    _LVL13_PIN = 10
+    
+    _LVL21_PIN = 11
+    _LVL22_PIN = 12
+    _LVL23_PIN = 13
+    
+    _COOL_PIN = 14
+    
     def __init__(self):
         Thread.__init__(self)
-        self._redPWM = PWMModule(pinNumber=self._redPin, duty_max=255)
-        self._greenPWM = PWMModule(pinNumber=self._greenPin, duty_max=255)
-        self._bluePWM = PWMModule(pinNumber=self._bluePin, duty_max=255)
-        self._buzzer = GPIOModule(pinNumber=self._buzzerPin)
-
+        self._redPWM = PWMModule(pinNumber=self._R_PIN, duty_max=255)
+        self._greenPWM = PWMModule(pinNumber=self._G_PIN, duty_max=255)
+        self._bluePWM = PWMModule(pinNumber=self._B_PIN, duty_max=255)
+        self._buzzer = GPIOModule(pinNumber=self._BUZZ_PIN)
+    
+    btn_wtrout_clicked = False
+    
+    def btn_wtrout_handle(self, pin):
+        self.btn_wtrout_clicked = True
 
     def setColorToPWM(self, color: str):
         try:
@@ -44,12 +75,13 @@ class ButtonIfaceThread(Thread):
     def run(self):
         self.main()
         
-    def buzzerBeep(self, delay):
+    def buzzerBeep(self, delay: int) -> None:
         self._buzzer.set_value(1)
         sleep(delay/1000)
         self._buzzer.set_value(0)
 
     def main(self):
         while True:
-
+            print('Buzzer run')
             asyncio.run(self.buzzerBeep(200))
+            sleep(0.1)
--- main.py
+++ main.py
@@ -6,80 +6,54 @@ app = Microdot()
 
 Response.default_content_type = 'text/html'
 
-# Our GPIO Module
-power = GPIOModule(23)   # Вкл/выкл
-cooler = PWMModule(22)   # Кулер
-volume = PWMModule(4)    # Звук
-brigtness = PWMModule(2) # Яркость
-
-yellow = GPIOModule(25)  # LED Yellow
-blue = GPIOModule(26)    # LED Blue
-red = GPIOModule(27)     # LED Red
-orange = GPIOModule(32)  # LED Orange
-green = GPIOModule(33)   # LED Green
-
-
-
 @app.route('/', methods=['GET'])
 async def index(request):
-    return render_template('index.html', work=power.get_value(), cool=cooler.get_value())
+    return render_template('index.html', work=False)
 
 @app.route('/yellow')
 async def toggle_power(request):
     print("Receive Yellow Toggle Request!")
-    yellow.toggle()
     return "OK"
 
 @app.route('/blue')
 async def toggle_power(request):
     print("Receive Blue Toggle Request!")
-    blue.toggle()
     return "OK"
 
 @app.route('/red')
 async def toggle_power(request):
     print("Receive Red Toggle Request!")
-    red.toggle()
     return "OK"
 
 @app.route('/orange')
 async def toggle_power(request):
     print("Receive Orange Toggle Request!")
-    orange.toggle()
     return "OK"
 
 @app.route('/green')
 async def toggle_power(request):
     print("Receive Green Toggle Request!")
-    green.toggle()
     return "OK"
 
 @app.route('/togglePower')
 async def toggle_power(request):
     print("Receive Power Toggle Request!")
-    power.toggle()
     return "OK"
 
 @app.route('/cooler', methods=['GET'])
 async def set_cooler(request):
     pwm_val = int(request.args['value'])
-    cooler.set_pwm(int(pwm_val/6*100))
-    print("Cooler:", cooler.get_value())
     return "OK"
 
 
 @app.route('/volume', methods=['GET'])
 async def set_volume(request):
     pwm_val = int(request.args['value'])
-    volume.set_pwm(pwm_val)
-    print("Volume:", volume.get_value())
     return "OK"
 
 @app.route('/brigtness', methods=['GET'])
 async def set_brigtness(request):
     pwm_val = int(request.args['value'])
-    brigtness.set_pwm(pwm_val)
-    print("Brightness:", brigtness.get_value())
     return "OK"
 
 @app.route('/shutdown')
@@ -91,7 +65,6 @@ async def shutdown(request):
 @app.route('/static/<path:path>')
 def static(request, path):
     if '..' in path:
-        # directory traversal is not allowed
         return 'Not found', 404
     return send_file('static/' + path)
 
